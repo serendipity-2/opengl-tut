056372e5b34160f92350c46db5f0cbd83a85dec5
serendipity-2 <81396183+serendipity-2@users.noreply.github.com>
2022-07-28 03:59:54 +0000

    Create utils.hpp

---
+++ common/utils.hpp
@@ -0,0 +1,16 @@
+// utils.hpp
+
+#ifndef __UTILS_H
+#define __UTILS_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+GLuint create_shader(const std::string, const GLuint);
+GLuint link_program(GLuint, GLuint);
+
+#endif 
